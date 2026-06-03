6660eb27615bf6017758e017038a14b7034bae7c
Ailton Domingues <ailton.domingues@nokia.com>
2026-06-03 20:22:27 -0300

    Use Nitro Vercel preset for proper SSR deployment

--- frontend/api/index.js
+++
@@ -1,6 +0,0 @@
-import { createRequestHandler } from '../dist/server/server.js';
-
-export default async function handler(req, res) {
-  const requestHandler = await createRequestHandler();
-  return requestHandler(req, res);
-}
